b30fa411875053efd29a23a6407daf8f438b9459
robert-min <robertmin522@gmail.com>
2023-05-29 14:35:00 +0900

    feat: add logging middleware

---
+++ chap4/logging_middleware/go.mod
@@ -0,0 +1,3 @@
+module github.com/robert-min/handson-go/chap4/logging_middleware
+
+go 1.19
---
+++ chap4/logging_middleware/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type LoggingClient struct {
+	log *log.Logger
+}
+
+// Logging outgoing requests
+func (c LoggingClient) RoundTrip(r *http.Request) (*http.Response, error) {
+	c.log.Printf("Sending a %s request to %s over %s\n", r.Method, r.URL, r.Proto)
+	resp, err := http.DefaultTransport.RoundTrip(r)
+	c.log.Printf("Got back a response over %s\n", resp.Proto)
+
+	return resp, err
+}
+
+func fetchRemoteResource(client *http.Client, url string) ([]byte, error) {
+	r, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+	return io.ReadAll(r.Body)
+}
+
+func createHTTPClientWithTimeout(d time.Duration) *http.Client {
+	client := http.Client{Timeout: d}
+	return &client
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stdout, "Must specify a HTTP URL to get data from")
+		os.Exit(1)
+	}
+
+	myTransport := LoggingClient{}
+	// Create log.Logger object, 로그가 작성될 io 객체
+	l := log.New(os.Stdout, "", log.LstdFlags)
+	myTransport.log = l
+
+	client := createHTTPClientWithTimeout(15 * time.Second)
+	client.Transport = &myTransport
+
+	body, err := fetchRemoteResource(client, os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stdout, "%#v\n", err)
+		os.Exit(1)
+	}
+	fmt.Fprintf(os.Stdout, "Bytes in response: %d\n", len(body))
+}
