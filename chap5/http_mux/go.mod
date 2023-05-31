e29767ed64efbaabeef8a43675b7c817fe8d95da
robert-min <robertmin522@gmail.com>
2023-05-31 21:06:08 +0900

    fix: http_mux add

---
+++ chap5/http_mux/go.mod
@@ -0,0 +1,3 @@
+module github.com/robert-min/handson-go/chap5/http_mux
+
+go 1.19
--- chap5/streaming_response/server.go
+++ chap5/streaming_response/server.go
@@ -9,6 +9,7 @@ import (
 	"time"
 )
 
+// longRunningProccess is example to send data
 func longRunningProcess(logWriter *io.PipeWriter) {
 	for i := 0; i <= 20; i++ {
 		fmt.Fprintf(logWriter, `{"id": %d, "user_ip": "172.121.19.21", "event": "click_on_add_cart" }`, i)
@@ -17,6 +18,7 @@ func longRunningProcess(logWriter *io.PipeWriter) {
 	}
 }
 
+// progresStreamer process response
 func progressStreamer(logReader *io.PipeReader, w http.ResponseWriter, done chan struct{}) {
 	buf := make([]byte, 500) // 특정 시점에 파이프에서 읽어 들일 최대 크기를 의미
 
@@ -26,7 +28,7 @@ func progressStreamer(logReader *io.PipeReader, w http.ResponseWriter, done chan
 
 	defer logReader.Close()
 	w.Header().Set("Content-Type", "text/plain")
-	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.Header().Set("X-Content-Type-Options", "nosniff") // 클라이언트 측에 데이터를 버퍼링하지 않도록 브라우저에 알림
 
 	for {
 		n, err := logReader.Read(buf)
@@ -45,7 +47,7 @@ func longRunningProcessHandler(w http.ResponseWriter, r *http.Request) {
 
 	done := make(chan struct{})
 	logReader, logWriter := io.Pipe()
-	go longRunningProcess(logWriter)
+	go longRunningProcess(logWriter) // 오래 걸리는 잡 실행
 	go progressStreamer(logReader, w, done)
 
 	<-done
